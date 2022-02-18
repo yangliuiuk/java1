0868de7d0fd685eb5d48c40771c5182e2c81ee19
yangliuiuk <89565497+yangliuiuk@users.noreply.github.com>
2022-02-18 10:59:19 -0500

    Update and rename PrimeNumber.java to PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,4 +1,9 @@
-public class PrimeNumber {
+/*
+Write a program to display the first 50 prime numbers. 
+An integer greater than 1 is prime if its only positive divisor is 1 or itself. 
+*/
+
+public class PrimeNumbers {
     public static void main(String[] args) {
         int count = 0;
         for (int number = 2; count < 50; number ++){
