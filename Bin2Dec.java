045d0621e0836013536ea429395c665dbbeb060e
yangliuiuk <89565497+yangliuiuk@users.noreply.github.com>
2022-09-20 00:17:18 -0400

    Update Bin2Dec.java

--- Bin2Dec.java
+++ Bin2Dec.java
@@ -1,6 +1,6 @@
 /*
-Write a program that reads a binary number from user, 
-then converts it to a decimal number and displays it. 
+Write a program that prompts the user to enter a binary number, 
+then converts it to a decimal number and prints it out. 
 Below is a sample run:
 Enter a binary number: 101
 The decimal number is: 5
@@ -11,11 +11,11 @@ public class Bin2Dec {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a binary number: ");
-        String binaryString = input.nextLine();
+        String binaryNumber = input.nextLine();
         int decimalNumber = 0;
         int base = 1;
-        for (int i = binaryString.length() - 1; i >= 0; i--){
-            if (binaryString.charAt(i) == '1')
+        for (int i = binaryNumber.length() - 1; i >= 0; i--){
+            if (binaryNumber.charAt(i) == '1')
                 decimalNumber += base;
             base *= 2;
         }
