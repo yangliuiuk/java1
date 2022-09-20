ccc5089c4c1833aea5b0e0c88f6f7141b0a18f51
yangliuiuk <89565497+yangliuiuk@users.noreply.github.com>
2022-09-19 23:25:32 -0400

    Create Dec2Hex.java

---
+++ Dec2Hex.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Dec2Hex {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a decimal number: ");
+        int decimalNumber = input.nextInt();
+        String hexString = "";
+        do 
+        {
+            int hexDigitNumber = decimalNumber % 16;
+            char hexDigitChar = '';
+            if (hexDigitNumber >= 0 && hexDigitNumber <= 9)
+                hexDigitChar = (char)(hexDigitNumber + '0');
+            else
+                hexDigitChar = (char)(hexDigitNumber - 10 + 'A');
+            hexString = hexDigitChar + hexString;
+            decimalNumber /= 16;
+        } while(decimalNumber > 0) ;
+        
+        System.out.println("The hex number is " + hexString);
+    }
+}
