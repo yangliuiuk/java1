30263773256a992d6f915311cbf9c0e20b42a7bc
yangliuiuk <89565497+yangliuiuk@users.noreply.github.com>
2022-09-22 03:39:35 -0400

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -1,6 +1,15 @@
 /*
 Write a program to display the first 50 prime numbers. 
 An integer greater than 1 is prime if its only positive divisor is 1 or itself. 
+
+Below is a sample run: 
+
+2 3 5 7 11 13 17 19 23 29 
+31 37 41 43 47 53 59 61 67 71 
+73 79 83 89 97 101 103 107 109 113 
+127 131 137 139 149 151 157 163 167 173 
+179 181 191 193 197 199 211 223 227 229
+
 */
 
 public class PrimeNumbers {
@@ -8,8 +17,8 @@ public class PrimeNumbers {
         int count = 0;
         for (int number = 2; count < 50; number ++){
             boolean isPrime = true;
-            for (int divisor = 1; divisor <= number; divisor ++ ){
-                if (number % divisor == 0 && divisor != 1 && divisor != number){
+            for (int divisor = 2; divisor <= number - 1; divisor ++ ){
+                if (number % divisor == 0){
                     isPrime = false;
                     break;
                 }
@@ -17,6 +26,9 @@ public class PrimeNumbers {
             if (isPrime){
                 System.out.print(number + " ");
                 count ++;
+                if (count % 10 == 0){
+                    System.out.println();
+                }
             }
         }
     }
